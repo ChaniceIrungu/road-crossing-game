34ce1bd7cb85545b0fbe40d1ed09b6acc582ff7e
Chanice Irungu <chaniceashley@gmail.com>
2022-01-06 18:46:10 +0300

    _randomised_enemy_speed_and_direction
    clean up

--- js/game.js
+++ js/game.js
@@ -3,7 +3,10 @@ let gameScene = new Phaser.Scene("Game");
 
 gameScene.init = function () {
   this.playerSpeed = 2;
-  this.dragonSpeed = 3;
+
+  //enemy speed
+  this.dragonMinSpeed = 2;
+  this.dragonMaxSpeed = 5;
 
   // boundaries
   this.dragonMinY = 40;
@@ -19,31 +22,31 @@ gameScene.preload = function () {
   this.load.image("treasure", "assets/treasure.png");
 };
 
-//called once after the preload ends
 gameScene.create = function () {
   //create bg sprite
   let bg = this.add.sprite(0, 0, "background");
-  // bg.setOrigin(0, 0);
   bg.setPosition(640 / 2, 360 / 2);
 
+  //player
   this.player = this.add.sprite(50, this.sys.game.config.height / 2, "player");
-  // player.setPosition(640 / 4, 360 / 2);
-  //set scale for changing size of sprite (can multiply h and w can be done by multiplying by 2, and half the size  * by 0.5)
   this.player.setScale(0.5);
 
+  //treasure
+  this.treasure = this.add.sprite(580, 180, "treasure");
+  this.treasure.setScale(0.5);
+
+  //enemy
   this.dragon = this.add.sprite(150, 180, "dragon");
   this.dragon.setScale(0.6);
   this.dragon.flipX = true;
 
-  this.dragon2 = this.add.sprite(400, 180, "dragon");
-  this.dragon2.displayWidth = 100;
-  this.dragon2.flipX = true;
+  //set enemy speed
+  let direction = Math.random() < 0.5 ? 1 : -1;
+  let speed =
+    this.dragonMinSpeed +
+    Math.random() * (this.dragonMaxSpeed - this.dragonMinSpeed);
 
-  this.dragon3 = this.add.sprite(500, 180, "dragon");
-  this.dragon3.angle = 45;
-
-  this.treasure = this.add.sprite(580, 180, "treasure");
-  this.treasure.setScale(0.5);
+  this.dragon.speed = direction * speed;
 };
 
 gameScene.update = function () {
@@ -63,23 +66,16 @@ gameScene.update = function () {
     this.scene.manager.bootScene(this);
   }
 
-  // if (this.dragon2.scaleX < 2) {
-  //   this.dragon2.scaleX += 0.01;
-  //   this.dragon2.scaleY += 0.01;
-  // }
-
-  this.dragon.y += this.dragonSpeed;
+  this.dragon.y += this.dragon.speed;
 
   //check we havent passed min or max Y
-  let conditionUp = this.dragonSpeed < 0 && this.dragon.y <= this.dragonMinY;
-  let conditionDown = this.dragonSpeed > 0 && this.dragon.y >= this.dragonMaxY;
+  let conditionUp = this.dragon.speed < 0 && this.dragon.y <= this.dragonMinY;
+  let conditionDown = this.dragon.speed > 0 && this.dragon.y >= this.dragonMaxY;
 
   //if the upper or lower limit is passed reverse
   if (conditionUp || conditionDown) {
-    this.dragonSpeed *= -1;
+    this.dragon.speed *= -1;
   }
-
-  this.dragon3.angle += 1;
 };
 
 //  configuration of the game
